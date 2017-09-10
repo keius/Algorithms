b7af538d4fd1ebf709e81b35de992355fb82ad3c
Greg <waterparkmelon@gmail.com>
2017-09-09 23:27:49 -0700

    lru

---
+++ Algorithms/LRUCache/lib/p01_int_set.rb
@@ -0,0 +1,99 @@
+class MaxIntSet
+  def initialize(max)
+    @store = Array.new(max, false)
+  end
+
+  def insert(num)
+    validate!(num)
+    @store[num] = true
+  end
+
+  def remove(num)
+    validate!(num)
+    @store[num] = false
+  end
+
+  def include?(num)
+    @store[num] == true
+  end
+
+  private
+
+  def is_valid?(num)
+    num.between?(0, @store.length - 1)
+  end
+
+  def validate!(num)
+    raise "Out of bounds" unless is_valid?(num)
+  end
+end
+
+
+class IntSet
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+  end
+
+  def insert(num)
+    self[num] << num
+  end
+
+  def remove(num)
+    self[num].delete(num)
+  end
+
+  def include?(num)
+    self[num].include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+    @store[num % 20]
+  end
+
+  def num_buckets
+    @store.length
+  end
+end
+
+class ResizingIntSet
+  attr_reader :count
+
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(num)
+    resize! if num_buckets == @count
+    self[num] << num
+    @count += 1
+  end
+
+  def remove(num)
+    self[num].delete(num)
+  end
+
+  def include?(num)
+    self[num].include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+    @store[(num % num_buckets)]
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+    old = @store
+    @store = Array.new(num_buckets * 2) {Array.new}
+    old.flatten.each { |num| self[num] << num}
+  end
+end
