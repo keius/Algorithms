3d4216b47c2580f34c67e58115f3862f7725fcec
Greg <waterparkmelon@gmail.com>
2017-09-10 14:05:42 -0700

    bit

---
+++ BitManipulation.java
@@ -0,0 +1,20 @@
+ArrayList<Integer> getAlternatingSequences(int n) {
+  ArrayList<Integer> sequences = new ArrayList<Integer>();
+
+  int searchingFor = 0;
+  int counter = 0;
+
+  for (int i = 0; i < Integer.BYTES * 8; i++) {
+    if ((n & 1) != searchingFor) {
+      sequences.add(counter);
+      searchingFor = n & 1;
+      counter = 0;
+    }
+    counter++;
+    n >>>= 1;
+  }
+
+  sequences.add(counter);
+
+  return sequences;
+}
