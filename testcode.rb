fc0c3c5184e4f2aab660d3336d3d2534002d0369
Greg <waterparkmelon@gmail.com>
2017-09-14 13:20:18 -0700

    test

--- testcode.rb
+++ testcode.rb
@@ -1,14 +1,27 @@
-def combs(n, arr)
-  results = [[]]
-  n.times do
-    results.length.times do
-      comb = results.shift
-      arr.each do |num|
-        results << (comb + [num])
+require 'byebug'
+
+def my_atoi(str)
+  debugger
+  negative = 1
+  number = 0
+  start = 0
+  (0...str.length).each do |i|
+      if str[i] == " "
+          start += 1
+      elsif str[i] == "-" || str[i] == "+"
+          negative = str[i] == "-" ? -1 : 1
+      elsif str[i].to_i.is_a? Integer
+          number *= 10
+          number += str[i].to_i
+      else
+          next
       end
-    end
   end
-  results
+
+  number *= negative
+  return 2147483647 if number > 2147483647
+  return -2147483648 if number < -2147483648
+  return number
 end
 
-print combs(3, [1,2,3])
+my_atoi("12")
