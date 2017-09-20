754aeb87ce8d357369da12d06681d007555ef1eb
Greg <waterparkmelon@gmail.com>
2017-09-20 10:24:58 -0700

    node

--- Nodestuff/github-grabber/animal_fun.js
+++ Nodestuff/github-grabber/animal_fun.js
@@ -1,4 +1,7 @@
 const fs = require('fs');
+const http = require('http');
+const qs = require('querystring');
+const cache = {};
 
 const selectAnimals = (animals, letter) => (
   animals.split('\n').filter(animal => animal.startsWith(letter)).join('\n')
@@ -21,3 +24,42 @@ fs.readFile('./animals.txt', 'utf-8', (err, data) => {
     }
   });
 });
+
+const animalServer = http.createServer((req, res) => {
+  const query = req.url.split('?')[1];
+  if (query !== undefined) {
+    const animalLetter = qs.parse(query).letter.toUpperCase();
+
+    if (cache[animalLetter] !== undefined) {
+      res.end(cache[animalLetter]);
+    }
+
+    if (animalLetter !== undefined) {
+      fs.readFile('./animals.txt', 'utf-8', (err, data) => {
+        if (err) {
+          console.log(err);
+          res.end('IT WENT POORLY');
+          return;
+        }
+        const animals = selectAnimals(data, animalLetter);
+        cache[animalLetter] = animals;
+        res.end(animals);
+      });
+    }
+  } else {
+    if (cache['animals'] !== undefined) {
+      res.end(cache['animals']);
+    }
+    fs.readFile('./animals.txt', 'utf-8', (err, data) => {
+      if (err) {
+        console.log(err);
+        res.end('IT WENT POORLY');
+        return;
+      }
+      cache['animals'] = data;
+      res.end(data);
+    });
+  }
+});
+
+animalServer.listen(8000, () => console.log("I'm listening on port 8000"));
