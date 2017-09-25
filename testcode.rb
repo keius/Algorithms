4401890a359dbf97a121d247ed64c95774b5629b
Greg <waterparkmelon@gmail.com>
2017-09-25 10:16:27 -0700

    test

--- testcode.rb
+++ testcode.rb
@@ -1,27 +1,26 @@
 require 'byebug'
 
-def my_atoi(str)
-  debugger
-  negative = 1
-  number = 0
-  start = 0
-  (0...str.length).each do |i|
-      if str[i] == " "
-          start += 1
-      elsif str[i] == "-" || str[i] == "+"
-          negative = str[i] == "-" ? -1 : 1
-      elsif str[i].to_i.is_a? Integer
-          number *= 10
-          number += str[i].to_i
-      else
-          next
-      end
-  end
+def merge(intervals)
+    return [] if intervals.empty?
 
-  number *= negative
-  return 2147483647 if number > 2147483647
-  return -2147483648 if number < -2147483648
-  return number
+    merged = []
+
+    intervals.sort! {|interval| interval.start}
+
+    left = intervals[0].start
+    right = intervals[0].end
+
+    intervals.each do |interval|
+        if interval.start.between?(left, right)
+            right = [right, interval.end].max
+        else
+            merged << [left, right]
+            left = interval.start
+            right = interval.end
+        end
+    end
+
+    merged << [left, right]
 end
 
-my_atoi("12")
+print can_jump([2,3,1,1,4])
