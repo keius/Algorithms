beee149704acf505e090bd2a9ef0c2207cc0094c
Greg <waterparkmelon@gmail.com>
2017-09-27 22:18:14 -0700

    expand city and search term

--- webscraper/craigslist_gig_scraper.rb
+++ webscraper/craigslist_gig_scraper.rb
@@ -1,21 +1,29 @@
 require 'open-uri'
 require 'nokogiri'
 
-url = "https://newyork.craigslist.org/search/cpg?query=developer&is_paid=all"
+cities = ['newyork']
+search_terms = ['developer']
 
-document = open(url)
-content = document.read
-parsed_content = Nokogiri::HTML(content)
+cities.each do |city|
+  search_terms.each do |search_term|
 
-parsed_content.css('.content').css('.result-row').each do |row|
-  title = row.css('.hdrlnk').inner_text
-  link = row.css('.hdrlnk').first.values[0]
-  posted_at = row.css('time').first.values[2]
-  hood = row.css('.result-info').css('.result-meta').css('.result-hood').first
-  neighborhood = hood ? hood.inner_text.strip : ""
+    url = "https://#{city}.craigslist.org/search/cpg?query=#{search_term}&is_paid=all"
 
-  puts "#{title} #{neighborhood}"
-  puts "Posted at #{posted_at}"
-  puts "#{link}"
-  puts '-------------------------------'
+    document = open(url)
+    content = document.read
+    parsed_content = Nokogiri::HTML(content)
+
+    parsed_content.css('.content').css('.result-row').each do |row|
+      title = row.css('.hdrlnk').inner_text
+      link = row.css('.hdrlnk').first.values[0]
+      posted_at = row.css('time').first.values[2]
+      hood = row.css('.result-info').css('.result-meta').css('.result-hood').first
+      neighborhood = hood ? hood.inner_text.strip : ""
+
+      puts "#{title} #{neighborhood}"
+      puts "Posted at #{posted_at}"
+      puts "#{link}"
+      puts '-------------------------------'
+    end
+  end
 end
