345eb2a0cdf538bb390364365ce50d1bc8a23b39
Greg <waterparkmelon@gmail.com>
2017-10-02 20:38:41 -0700

    wiki_crawler

--- webscraper/wiki_crawler.rb
+++ webscraper/wiki_crawler.rb
@@ -1,10 +1,13 @@
 require 'open_uri_redirections'
 require 'nokogiri'
+require 'fileutils'
 
 DATA_DIR = "data-hold/nobel"
-Dir.mkdir(DATA_DIR) unless File.exists?(DATA_DIR)
+FileUtils.mkdir_p(DATA_DIR) unless File.exists?(DATA_DIR)
 
-BASE_WIKIPEDIA_URL = "http://en.wikipedia.org"
+HEADERS_HASH = {"User-Agent" => "Ruby/#{RUBY_VERSION}"}
+
+BASE_WIKIPEDIA_URL = "https://en.wikipedia.org"
 LIST_URL = "#{BASE_WIKIPEDIA_URL}/wiki/List_of_Nobel_laureates"
 
 page = Nokogiri::HTML(open(LIST_URL, :allow_redirections => :safe))
@@ -16,5 +19,19 @@ rows[1..-2].each do |row|
   hrefs.each do |href|
     remote_url = BASE_WIKIPEDIA_URL + href
     local_fname = "#{DATA_DIR}/#{File.basename(href)}.html"
+    unless File.exists?(local_fname)
+      puts "Fetching #{remote_url}..."
+      begin
+        wiki_content = open(remote_url, HEADERS_HASH).read
+      rescue Exception=>e
+        puts "Error: #{e}"
+        sleep 5
+      else
+        File.open(local_fname, 'w'){|file| file.write(wiki_content)}
+        puts "\t...Success, saved to #{local_fname}"
+      ensure
+        sleep 1.0 + rand
+      end  # done: begin/rescue
+    end # done: unless File.exists?
   end
 end
