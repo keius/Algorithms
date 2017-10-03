94421da724d7b4fdc6b28ab11f90965d3a4b8677
Greg <waterparkmelon@gmail.com>
2017-10-03 15:51:00 -0700

    test

--- testcode.rb
+++ testcode.rb
@@ -1,10 +1,21 @@
 require 'byebug'
 
-def subsets(arr)
-  return [[]] if arr.empty?
-  (0..arr.length - 1).each do |i|
-    print arr.take(i) + arr.drop(i+1)
-  end
+def unique_paths_with_obstacles(obstacle_grid)
+    m = obstacle_grid.length
+    n = obstacle_grid[0].length
+    graph = Array.new(m) { Array.new(n) }
+    graph[0][0] = obstacle_grid[0][0] == 1 ? 0 : 1
+    print graph
+
+    (0...m).each do |row|
+        (0...n).each do |col|
+            left = graph[row - 1][col] || 0
+            top = graph[row][col - 1] || 0
+            graph[row][col] = obstacle_grid[row][col] == 1 ? 0 : left + top
+        end
+    end
+
+    graph[m - 1][n - 1]
 end
 
-print subsets([1, 2])
+print unique_paths_with_obstacles([[0]])
