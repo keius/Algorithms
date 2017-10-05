e1e4876d3d866c8088e7cab5742c3c3e4c67d99d
Greg <waterparkmelon@gmail.com>
2017-10-04 20:12:37 -0700

    tesT

--- testcode.rb
+++ testcode.rb
@@ -1,21 +1,17 @@
 require 'byebug'
 
-def unique_paths_with_obstacles(obstacle_grid)
-    m = obstacle_grid.length
-    n = obstacle_grid[0].length
-    graph = Array.new(m) { Array.new(n) }
-    graph[0][0] = obstacle_grid[0][0] == 1 ? 0 : 1
-    print graph
+def magic_index(arr, start_idx = 0, end_idx = 0)
+  return false if arr.length < 1
+  mid = start_idx + end_idx / 2
 
-    (0...m).each do |row|
-        (0...n).each do |col|
-            left = graph[row - 1][col] || 0
-            top = graph[row][col - 1] || 0
-            graph[row][col] = obstacle_grid[row][col] == 1 ? 0 : left + top
-        end
-    end
-
-    graph[m - 1][n - 1]
+  case arr[mid] <=> mid
+  when 0
+    return true
+  when 1
+    magic_index(arr, mid + 1, end_idx)
+  when -1
+    magic_index(arr, start_idx, mid - 1)
+  end
 end
 
-print unique_paths_with_obstacles([[0]])
+print magic_index([1, 2, 3, 9])
