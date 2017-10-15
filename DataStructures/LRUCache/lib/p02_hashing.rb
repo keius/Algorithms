725b3aeb7fa2d68ade764b53c028df5fd892e84d
Greg <waterparkmelon@gmail.com>
2017-10-15 14:09:48 -0700

    hashing

--- DataStructures/LRUCache/lib/p02_hashing.rb
+++ DataStructures/LRUCache/lib/p02_hashing.rb
@@ -4,11 +4,15 @@ end
 
 class Array
   def hash
+    self.each_with_index.reduce(0) do |acc, (el, i)|
+      (el.hash + i.hash) ^ acc
+    end
   end
 end
 
 class String
   def hash
+    self.chars.map(&:ord).hash
   end
 end
 
@@ -16,5 +20,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
+    to_a.sort_by(&:hash).hash
   end
 end
