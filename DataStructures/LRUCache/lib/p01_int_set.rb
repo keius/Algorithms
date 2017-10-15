4b75f840fdca86124c7850ec6f535dda0da07008
Greg <waterparkmelon@gmail.com>
2017-10-14 23:19:02 -0700

    intset

--- DataStructures/LRUCache/lib/p01_int_set.rb
+++ DataStructures/LRUCache/lib/p01_int_set.rb
@@ -32,24 +32,29 @@ end
 
 class IntSet
   def initialize(num_buckets = 20)
-
+    @store = Array.new(num_buckets) {Array.new}
   end
 
   def insert(num)
+    self[num] << num
   end
 
   def remove(num)
+    self[num].delete(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
+    @store[num % 20]
   end
 
   def num_buckets
+    @store.length
   end
 end
 
