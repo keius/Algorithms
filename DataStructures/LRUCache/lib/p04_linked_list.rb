b7853d1dd08007a5061a125306f46ad0d1d9c629
Greg <waterparkmelon@gmail.com>
2017-10-15 21:32:15 -0700

    hashmap

--- DataStructures/LRUCache/lib/p04_linked_list.rb
+++ DataStructures/LRUCache/lib/p04_linked_list.rb
@@ -2,7 +2,10 @@ class Node
   attr_accessor :key, :val, :next, :prev
 
   def initialize(key = nil, val = nil)
-
+    @key = key
+    @val = val
+    @next = nil
+    @prev = nil
   end
 
   def to_s
@@ -10,7 +13,9 @@ class Node
   end
 
   def remove
-
+    @prev.next = @next
+    @next.prev = @prev
+    @next, @prev = nil, nil
   end
 end
 
@@ -18,43 +23,79 @@ class LinkedList
   include Enumerable
 
   def initialize
-
+    @head = Node.new
+    @tail = Node.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
-
+    node = first
+    i.times do |i|
+      node = node.next
+    end
+    node
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
+    each do |node|
+      return node.val if node.key == key
+    end
+
+    nil
   end
 
   def include?(key)
-
+    get(key) ? true : false
   end
 
   def append(key, val)
-
+    new_node = Node.new(key, val)
+    last_node = last
+    last_node.next = new_node
+    new_node.prev = last_node
+    new_node.next = @tail
+    @tail.prev = new_node
   end
 
   def update(key, val)
-
+    each do |node|
+      if node.key == key
+        node.val = val
+        break
+      end
+    end
+    nil
   end
 
   def remove(key)
-
+    each do |node|
+      if node.key == key
+        node.remove
+        return node
+      end
+    end
+    nil
   end
 
   def each
-
+    node = first
+    until node == @tail
+      yield(node)
+      node = node.next
+    end
   end
 
   def to_s
--- DataStructures/LRUCache/lib/p05_hash_map.rb
+++ DataStructures/LRUCache/lib/p05_hash_map.rb
@@ -7,34 +7,45 @@ class HashMap
   attr_reader :count
 
   def initialize(num_buckets = 8)
-
+    @count = 0
+    @store = Array.new(num_buckets) {LinkedList.new}
   end
 
   def include?(key)
+    bucket(key).include?(key)
   end
 
   def set(key, val)
-
+    resize! if @count == @store.length
+    if include?(key)
+      bucket(key).update(key, val)
+    else
+      bucket(key).append(key, val)
+      @count += 1
+    end
   end
 
   def get(key)
+    bucket(key).get(key)
   end
 
   def delete(key)
-
+    bucket(key).remove(key)
+    @count -= 1
   end
 
   def each
-
+    @store.each do |list|
+      list.each {|node| yield(node.key, node.val)}
+    end
   end
 
-  # uncomment when you have Enumerable included
-  # def to_s
-  #   pairs = inject([]) do |strs, (k, v)|
-  #     strs << "#{k.to_s} => #{v.to_s}"
-  #   end
-  #   "{\n" + pairs.join(",\n") + "\n}"
-  # end
+  def to_s
+    pairs = inject([]) do |strs, (k, v)|
+      strs << "#{k.to_s} => #{v.to_s}"
+    end
+    "{\n" + pairs.join(",\n") + "\n}"
+  end
 
   alias_method :[], :get
   alias_method :[]=, :set
@@ -42,13 +53,21 @@ class HashMap
   private
 
   def num_buckets
+    @store.length
   end
 
   def resize!
-
+    old_store = @store
+    @store = Array.new(num_buckets * 2) {LinkedList.new}
+    @count = 0
+    old_store.each do |list|
+      list.each do |node|
+        set(node.key, node.val)
+      end
+    end
   end
 
   def bucket(key)
-
+    @store[key.hash % num_buckets]
   end
 end
