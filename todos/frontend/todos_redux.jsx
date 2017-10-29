d8c10846f0c41fdda0b532f9da2725a0342dfb27
Greg <waterparkmelon@gmail.com>
2017-10-28 23:54:32 -0700

    store

---
+++ todos/frontend/todos_redux.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('content');
+  const store = configureStore();
+
+  window.store = store;
+
+  ReactDOM.render(<h1>TodoList App</h1>, root);
+});
