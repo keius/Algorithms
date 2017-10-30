19b03cf64e60b5321507e649f67de949af50c17f
Greg <waterparkmelon@gmail.com>
2017-10-29 18:10:03 -0700

    testing redux

--- todos/frontend/reducers/todos_reducer.js
+++ todos/frontend/reducers/todos_reducer.js
@@ -1,5 +1,18 @@
+import { RECEIVE_TODOS, RECEIVE_TODO } from '../actions/todos_actions';
+import merge from 'lodash/merge';
+
 const todosReducer = (state = {}, action) => {
+  Object.freeze(state);
+  let nextState;
+
   switch(action.type) {
+    case RECEIVE_TODOS:
+      nextState = {};
+      action.todos.forEach (todo => { nextState[todo.id] = todo; });
+      return nextState;
+    case RECEIVE_TODO:
+      const newTodo = {[action.todo.id]: action.todo};
+      return merge({}, state, newTodo);
     default:
       return state;
     }
--- todos/frontend/todos_redux.jsx
+++ todos/frontend/todos_redux.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import {receiveTodos, receiveTodo} from './actions/todos_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('content');
   const store = configureStore();
 
   window.store = store;
+  window.receiveTodo = receiveTodo;
 
   ReactDOM.render(<h1>TodoList App</h1>, root);
 });
